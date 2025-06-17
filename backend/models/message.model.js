688803991ccd35544f3137dec87f73ee29dd9cb6
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-17 13:51:02 +0530

    Update message.model.js to handle the storage of the chats

---
+++ backend/models/message.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'project',
+        required: true
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Message = mongoose.model('message', messageSchema);
+
+export default Message;
