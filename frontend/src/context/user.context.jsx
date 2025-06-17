a274c1cf441a1d791a5f723baa7efce65caa75ec
Tushar Sharma <tusharsharma2822@gmail.com>
2025-06-17 22:39:13 +0530

    Entire Frontend Commit

--- frontend/src/context/user.context.jsx
+++ frontend/src/context/user.context.jsx
@@ -1,12 +1,22 @@
-import React, { createContext, useState, useContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import axios from '../config/axios';
 
 // Create the UserContext
 export const UserContext = createContext();
 
 // Create a provider component
 export const UserProvider = ({ children }) => {
-    const [ user, setUser ] = useState(null);
-
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            axios.get('/users/me')
+                .then(res => setUser(res.data.user))
+                .catch(() => setUser(null));
+        } else {
+            setUser(null);
+        }
+    }, []);
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
